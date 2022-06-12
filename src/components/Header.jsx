aadc26e5420bf0460d5d9a13bc8cb4efffbf249e
kartik <kartikth40@gmail.com>
2022-06-12 12:54:23 +0530

    added basic pages and stylized-components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = () => {
+  return <HeaderDiv>Header</HeaderDiv>
+}
+
+export default Header
+
+const HeaderDiv = styled.header`
+  font-weight: bold;
+  font-size: 30px;
+`
---
+++ src/pages/RegisterBuyer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const RegisterBuyer = () => {
+  return <div>RegisterBuyer</div>
+}
+
+export default RegisterBuyer
